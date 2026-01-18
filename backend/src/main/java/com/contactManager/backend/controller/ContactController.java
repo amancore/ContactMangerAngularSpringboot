7f91e921f4ef4c14a387428fcce17ae51b4d9319
Amancore__ <aman8804623822@gmail.com>
2026-01-18 12:35:21 +0530

    updated the comments structured with explanation

--- backend/src/main/java/com/contactManager/backend/controller/ContactController.java
+++ backend/src/main/java/com/contactManager/backend/controller/ContactController.java
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequiredArgsConstructor
-@RestController
-@RequestMapping("/api/contacts")
-@CrossOrigin(origins = "http://localhost:4200")
+@RequiredArgsConstructor // auto generates constructor for final fields
+@RestController // returns the json auto [ not html ]
+@RequestMapping("/api/contacts") // comman base url for the all api's
+@CrossOrigin(origins = "http://localhost:4200") // allow frontend to call the backend
 public class ContactController {
     private final ContactService contactService;
 
@@ -20,18 +20,47 @@ public class ContactController {
     public ResponseEntity<List<ContactDto>> getAllContacts(){
         return ResponseEntity.ok(contactService.getAllContacts());
     }
+    // ResponseEntity : return body + status code + headers
+    //Controller calls contactService.getAllContacts()
+    //Service fetches from DB using repository
+    //Converts List<Contact> → List<ContactDto>
+    //Returns list to controller
+
     @GetMapping("/{id}")
     public ResponseEntity<ContactDto> getContactById(@PathVariable Long id){
         return ResponseEntity.ok(contactService.getContactById(id));
     }
+    // extracts id = 5
+    //Controller sends id to service
+    //Service does findById
+    //If found → convert to DTO
+    //If not found → exception
+
+
+
     @PostMapping
     public ResponseEntity<ContactDto> createContact (@Valid @RequestBody ContactDto dto){
         return ResponseEntity.ok(contactService.createContact(dto));
     }
+    // @Requestbody ContactDto dto [spring converts json -> contactDto auto]
+    // @valid - > for not null, not blank, if fails returns 400 bad request
+    // Controller receives DTO
+    // calls service.createContact(dto)
+    // service converts DTO → Entity
+    // repo saves in DB
+    // service converts saved entity → DTO
+    // controller returns DTO JSON
+
     @PutMapping("/{id}")
     public ResponseEntity<ContactDto> updateContact (@PathVariable Long id, @RequestBody ContactDto dto){
         return ResponseEntity.ok(contactService.updateContact(id, dto));
     }
+    // controller sends id + dto to service
+    // service finds existing row in DB
+    // updates fields
+    // saves again
+    // returns updated DTO
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteContact(@PathVariable Long id){
         contactService.deleteContact(id);
--- backend/src/main/java/com/contactManager/backend/service/Impl/ContactServiceImpl.java
+++ backend/src/main/java/com/contactManager/backend/service/Impl/ContactServiceImpl.java
@@ -6,32 +6,70 @@ import com.contactManager.backend.mapper.ModelMapper;
 import com.contactManager.backend.repository.ContactRepository;
 import com.contactManager.backend.service.ContactService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
+//creates its object auto (bean), controller can inject and use it
+@RequiredArgsConstructor // lombok generates constructor auto for *final fields, no need to write the constructor manually
 public class ContactServiceImpl implements ContactService {
     private final ContactRepository contactRepository;
+    //db connector
+    // provides method like (findAll(), findbyId(id), save(entity), deletebyId(id))
+    // all are coming from the JpaRespository
+
     @Override
     public List<ContactDto> getAllContacts() {
         return contactRepository.findAll().stream().map(ModelMapper::toDto).toList();
     }
+    // db fetch .findAll() -[select * from contact;]
+    // output List<Contact> (entities)
+    // convert entites into list Dto [.stream().map(ModelMapper::toDto())
+    // each row of the *Contact -> converted into *ContactDto
+    // Output : List<ContactDto>
+    // Service returo Dto list to controller
+    // controller returns its as JSON reponse to frontend
 
     @Override
     public ContactDto getContactById(Long id){
         Contact contact = contactRepository.findById(id).orElseThrow(()-> new RuntimeException("Contact Not Found"));
         return ModelMapper.toDto(contact);
     }
+    // input : id , ex : GET/contact/5
+    // id = 5, passed to the service
+    // find the db : .findById(5), [select * from contact where id =5;]
+    // return Optional<Contact> : [because maybe id exists or maybe not]
+    // if not found .ElseThrow(...)
+    // if exists : Convert entity(contact) -> Dto(ContactDto)
+    // ModelMapper.toDto(contact)
+    // return ContactDto
+
     @Override
     public ContactDto createContact(ContactDto dto){
         Contact contact = ModelMapper.toEntity(dto);
         return ModelMapper.toDto(contactRepository.save(contact));
     }
+    // input : ContactDto dto
+    // this comes from the api body : POST/contacts
+    // json :  { "name" : "Aman" , "email" : "aman@gmail.com", "phone": "999"}
+    // spring auto convert json->Dto
+    // convert dto -> entity [Contact contact = ModelMapper.toEntity(dto);
+    // because repository works with the entity not with the dto
+    // save in the db, contactRepository.save(contact);
+    // sql : [insert into contact(name,emai, phone), values("aman"...);
+    // db generates ID auto (because @generated value)
+    // so now entity becomes : Contact(id=10, name="Aman"...)
+    // now save contact updated :
+    // convert saved Entity -> Dto
+    // ModelMapper.toDto(savedEntity) , frontend gets back Dto including id
+    // return type ContactDto :
+    // {
+    //  "id": 10,
+    //  "name": "Aman",
+    //  "email": "aman@gmail.com",
+    //  "phone": "99999"
+    //}
 
     @Override
     public ContactDto updateContact(Long id, ContactDto contactDto) {
@@ -42,6 +80,27 @@ public class ContactServiceImpl implements ContactService {
         Contact updated = contactRepository.save(existing);
         return ModelMapper.toDto(updated);
     }
+    // input : LONG id, ContactDto (from request body)
+    // PUT/contacts/10
+    // json : {
+    //  "name": "Aman Raj",
+    //  "email": "amanraj@gmail.com",
+    //  "phone": "88888"
+    //}
+    // check record exits : contactRepository.findById(id); [select * from contact where id = 10;]
+    // if not found then error
+    // object exists has updated values
+    // save again : contactRepository.save(existing)
+    //UPDATE contact
+    //SET name="Aman Raj",
+    //    email="amanraj@gmail.com",
+    //    phone="88888"
+    //WHERE id=10;
+
+    // now convert entity -> dto; [modelMapper.toDto(updated)
+    // return type ContactDto
+
+
 
     @Override
     public void deleteContact(Long id) {
